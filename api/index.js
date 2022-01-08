0970711dc12ef443f153316489cb95cda4eb3442
Qarola <cyan.323@gmail.com>
2022-01-08 18:05:16 -0500

    added procfile

--- api/index.js
+++ api/index.js
@@ -60,7 +60,7 @@ conn.sync({ force: false }).then(() => {
    */
 
   console.log("Connection with DB is correct");
-  server.listen(3001, () => {
+  server.listen(process.env.PORT || 3001, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
